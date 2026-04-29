679bf496be7742b3bbaec97f2af4fad12780abf7
Doffy <doffy.fastcodingvn@gmail.com>
2026-04-29 15:27:50 +0700

    [upd] auth service boilerplate

---
+++ auth/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const { json } = bodyParser;
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

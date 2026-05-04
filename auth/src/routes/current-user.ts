25128a0b28f6c7e317298cea0aae9309614f0fe3
Doffy <doffy.fastcodingvn@gmail.com>
2026-05-04 17:56:02 +0700

    [fix] prettier

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,12 +3,8 @@ import { getCurrentUserHandler } from '../middleware/get-current-user-handler.ts
 
 const router = express.Router();
 
-router.get(
-  '/api/users/currentuser',
-  getCurrentUserHandler,
-  (req, res) => {
-    return res.send({ currentUser: req.currentUser });
-  },
-);
+router.get('/api/users/currentuser', getCurrentUserHandler, (req, res) => {
+  return res.send({ currentUser: req.currentUser });
+});
 
 export { router as currentUserRouter };

49b2b457b0a709b69de9a57b397076b4e5719d98
Doffy <doffy.fastcodingvn@gmail.com>
2026-05-11 18:12:55 +0700

    [fix] dir

--- ticket/src/config.ts
+++ ticket/src/config.ts
@@ -1,3 +1,7 @@
 export const COOKIE_NAME = process.env.COOKIE_NAME;
 export const COOKIE_KEY = process.env.COOKIE_KEY;
 export const JWT_KEY = process.env.JWT_KEY;
+
+console.log(COOKIE_NAME);
+console.log(COOKIE_KEY);
+console.log(JWT_KEY);

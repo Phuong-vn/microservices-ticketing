2fa24f1c6e7c1c0d4b9228c2be8703e980b31ae4
Doffy <doffy.fastcodingvn@gmail.com>
2026-05-11 19:10:01 +0700

    [fix] response data

--- ticket/src/routes/details.ts
+++ ticket/src/routes/details.ts
@@ -11,7 +11,11 @@ router.get('/api/tickets/:id', async (req: Request, res: Response) => {
   if (!ticket) {
     throw new NotFoundError();
   }
-  return res.send(ticket);
+  return res.send({
+    id: ticket._id,
+    title: ticket.title,
+    price: ticket.price,
+  });
 });
 
 export { router as detailsRouter };

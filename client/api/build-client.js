8198983a1edf69f05d6518cebbf01ba65f5094ee
Doffy <doffy.fastcodingvn@gmail.com>
2026-05-11 14:42:05 +0700

    [fix] context

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const buildClient = ({ req }) => {
+const buildClient = (context) => {
   if (typeof window === 'undefined') {
     return axios.create({
       baseURL:
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers,
+      context,
     });
   }
   return axios;

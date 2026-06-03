e9b0b3a9bd596728c7263e0dafb5f4aa6795e768
Doffy_FASTCODINGVN <doffy.fastcodingvn@gmail.com>
2026-06-03 22:31:48 +0700

    [upd] nats wrapper

--- ticket/src/index.ts
+++ ticket/src/index.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { randomUUID } from 'crypto';
 import { app } from './app.ts';
+import { natsWrapper } from './natsWrapper.ts';
 
 const port = 3000;
 
 await mongoose.connect('mongodb://ticket-mongo-srv:27017');
-
+await natsWrapper.connect('ticketing', randomUUID(), 'http://nats-srv:4222')
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
---
+++ ticket/src/natsWrapper.ts
@@ -0,0 +1,17 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+  private _client?: Stan;
+  connect(clusterId: string, clientId: string, url: string) {
+    return new Promise<void>((resolve, reject) => {
+      this._client = nats.connect(clusterId, clientId, { url });
+      this._client.on('connect', () => {
+        console.log('nats is connected')
+        resolve();
+      });
+      this._client.on('error', (err) => reject(err));
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();

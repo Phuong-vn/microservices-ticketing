a8208a852f071a1c816d19140ea043c9b9c7bc8c
Doffy <doffy.fastcodingvn@gmail.com>
2026-06-23 12:30:28 +0700

    [upd] common

--- common/src/middleware/validation-handler.ts
+++ common/src/middleware/validation-handler.ts
@@ -1,15 +1,10 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/index.js';
 
-export const validationHandler = (
-  req: Request,
-  _res: Response,
-  next: NextFunction,
-) => {
+export const validationHandler = (req: Request, _res: Response) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
-  next();
 };
